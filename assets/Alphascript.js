2242f94a2673979fbacf998ec3a27a2175213bf7
ZRobertson27 <115191138+ZRobertson27@users.noreply.github.com>
2022-11-17 14:20:38 -0600

    Trying to structure javascript.

--- assets/Alphascript.js
+++ assets/Alphascript.js
@@ -1,19 +1,22 @@
 let goButton = document.getElementById('goButton')
 let quizContainer = document.getElementById('quizContainer')
-let timerEl = document.getElementById('countdown')
 let htmlQuestion = document.getElementById('question')
-let question
 let answerButtons = document.getElementById('answer-buttons')
+let randomQuestion 
+let currentQuestion
+let question
+let answers
 let questions = [
+    
     {
-        question: "var fruits = [apples, bannanas, pears, oranges] Which of the following will pring 'pears' to the console?",
+        question: "var fruits = [apples, bannanas, pears, oranges]. Which of the following will print 'pears' to the console?",
         answers: [
             {text: 'console.log(fruits[1]);', correct: false},
             {text: 'console.log(fruits[2]);', correct: true},
             {text: 'console.log(fruits[3]);', correct: false},
             {text: 'console.log(fruits[4]);', correct: false}
         ]
-    }, 
+    },
     {
         question: "An array can only store numbers.",
         answers: [
@@ -66,46 +69,43 @@ let questions = [
             {text: '', correct: ""},
             {text: '', correct: ""}
         ]
-    }
+    },
 ]
 
-// Starts quiz and timer and clicking Go.
+
+
 goButton.addEventListener('click', startQuiz)
+//Function to start quiz
+    //Question+answers appear
+    //Timer starts counting down
+    //function ends when all questions are answered or timer hits 0.
 
 function startQuiz() {
     console.log("The quiz has begun!");
-    startTimer()
     goButton.classList.add('hide');
     randomQuestion = questions.sort(() => Math.random() - .5);
+    console.log(randomQuestion);
     currentQuestion = 0;
     quizContainer.classList.remove('hide');
-    showNextQuestion()
-}
-// Time counts down from 75 seconds to 1 second. Stops at 0.
-function startTimer() {
-    var timeLeft = 75;
-
-    var timeInterval = setInterval(function () {
-        if (timeLeft > 1) {
-            timerEl.textContent = timeLeft + ' seconds remaining!';
-            timeLeft--;
-        } else if (timeLeft===1) {
-            timerEl.textContent = timeLeft + ' second remaining!'
-            timeLeft--;
-        } else {
-            timerEl.textContent = timeLeft;
-        }
-    }, 1000);
+    setNextQuestion();
 }
 
-function showNextQuestion() {
+function setNextQuestion() {
+    resetState()
     theQuestion(randomQuestion[currentQuestion])
+}
+
+function resetState () {
+    
+    while (answerButtonsElement.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild)
+    }
 }
 
 function theQuestion(question) {
     htmlQuestion.innerText = question.question
     question.answers.forEach(answers => {
-        let button = document.createElement('button')
+        var button = document.createElement('button')
         button.innerText = answers.text
         button.classList.add('button')
         if (answers.correct) {
@@ -117,19 +117,5 @@ function theQuestion(question) {
 }
 
 function chooseAnswer(e) {
-    console.log("Answer selected");
-    if (answers.correct) {
-        console.log("Wrong");
-    }
+
 }
-
-
-
-
-
-
-
-
-
-
-
